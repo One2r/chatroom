e6e51b4d10b0160b8f663408c8c99ea7e997c09c
One2r <601941036@qq.com>
2017-06-09 14:28:05 +0800

    添加jwt

---
+++ library/jwt/jwt.go
@@ -0,0 +1,24 @@
+package jwt
+
+import (
+	"fmt"
+
+	"github.com/astaxie/beego"
+	"github.com/dgrijalva/jwt-go"
+)
+
+//验证token
+func CheckToken(tokenStr string) (*jwt.Token, error) {
+	var errorVar error
+
+	if len(tokenStr) == 0 {
+		errorVar = fmt.Errorf("请输入token")
+	}
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte(beego.AppConfig.String("jwt_secret")), nil
+	})
+	if err != nil {
+		errorVar = fmt.Errorf("无效token")
+	}	
+	return token,errorVar
+}
