ea7e68b601bb935061e333d23c370dbd71cf8b52
One2r <601941036@qq.com>
2017-06-27 17:28:51 +0800

    添加房管接口控制器

--- routers/router.go
+++ routers/router.go
@@ -12,9 +12,15 @@ func init() {
 	beego.Router("/", &chat.WebSocketController{}, "get:Join")
 
 	//chatroom后台管理接口路由
-	ns := beego.NewNamespace("/admin",
+	adminNS := beego.NewNamespace("/admin",
 		beego.NSRouter("/sensitive/update", &admin.AdminController{}, "get:UpdateSensitiveWords"),
 		beego.NSRouter("/replace/update", &admin.AdminController{}, "get:UpdateReplaceWords"),
 	)
-	beego.AddNamespace(ns)
+	beego.AddNamespace(adminNS)
+
+	//chatroom后台房管接口路由
+	managerNS := beego.NewNamespace("/openapi",
+		beego.NSRouter("/room/silence", &admin.ManagerController{}, "get:SetRoomSilence"),
+	)
+	beego.AddNamespace(managerNS)
 }
