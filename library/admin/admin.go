20ffe9d64b757019c4b189a1accc61b2f875d542
One2r <601941036@qq.com>
2017-07-04 17:33:29 +0800

    dashboard页数据统计

--- library/admin/admin.go
+++ library/admin/admin.go
@@ -1,1 +1,19 @@
 package admin
+
+import (
+	"chatroom/models"
+
+	"github.com/astaxie/beego"
+)
+
+//GetStatis 获取聊天室相关统计信息
+func GetStatis() map[string]interface{} {
+	statis := make(map[string]interface{})
+	statis["online"] = 0
+	statis["roomNum"] = len(models.Subscribers)
+	for _, room := range models.Subscribers {
+		statis["online"] = statis["online"].(int) + room.Len()
+	}
+	beego.Info(statis)
+	return statis
+}
