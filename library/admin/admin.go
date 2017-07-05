a5c4fe9798549d0f6c10b44be5661317a9aed1d8
One2r <601941036@qq.com>
2017-07-05 10:34:09 +0800

    dashboard页面数据统计

--- library/admin/admin.go
+++ library/admin/admin.go
@@ -2,18 +2,30 @@ package admin
 
 import (
 	"chatroom/models"
-
-	"github.com/astaxie/beego"
 )
 
 //GetStatis 获取聊天室相关统计信息
 func GetStatis() map[string]interface{} {
 	statis := make(map[string]interface{})
 	statis["online"] = 0
+	statis["MaxOnline"] = 0
 	statis["roomNum"] = len(models.Subscribers)
-	for _, room := range models.Subscribers {
-		statis["online"] = statis["online"].(int) + room.Len()
+	if statis["roomNum"].(int) > 0 {
+		rooms := make(map[int]map[string]int)
+		for k, room := range models.Subscribers {
+			statis["online"] = statis["online"].(int) + room.Len()
+			statis["MaxOnline"] = statis["MaxOnline"].(int) + models.Roomconf[k].MaxOnline
+			roomInfo := make(map[string]int)
+			roomInfo["online"] = room.Len()
+			if models.Roomconf[k].Silence {
+				roomInfo["Silence"] = 1
+			} else {
+				roomInfo["Silence"] = 0
+			}
+			roomInfo["MaxOnline"] = models.Roomconf[k].MaxOnline
+			rooms[k] = roomInfo
+		}
+		statis["rooms"] = rooms
 	}
-	beego.Info(statis)
 	return statis
 }
