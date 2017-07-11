c0f2d020404ec34de7e1985a31c6347589f13941
One2r <601941036@qq.com>
2017-07-11 11:01:56 +0800

    管理后台dashboard页统计调整

--- library/admin/admin.go
+++ library/admin/admin.go
@@ -2,34 +2,44 @@ package admin
 
 import (
 	"chatroom/models"
+	"strings"
 
-	"github.com/astaxie/beego"
+	"strconv"
+
 	"github.com/garyburd/redigo/redis"
 )
 
 //GetStatis 获取聊天室相关统计信息
 func GetStatis() map[string]interface{} {
 	redisConn := models.RedisConnPool.Get()
-	v, _ := redis.StringMap(redisConn.Do("PUBSUB", "CHANNELS", "chat_room_*"))
-	beego.Info(v)
+	chatRoom, _ := redis.Strings(redisConn.Do("PUBSUB", "CHANNELS", "chat_room_*"))
 	statis := make(map[string]interface{})
 	statis["online"] = 0
 	statis["MaxOnline"] = 0
-	statis["roomNum"] = len(models.Subscribers)
+	statis["roomNum"] = len(chatRoom)
 	if statis["roomNum"].(int) > 0 {
 		rooms := make(map[int]map[string]int)
-		for k, room := range models.Subscribers {
-			statis["online"] = statis["online"].(int) + room.Len()
-			statis["MaxOnline"] = statis["MaxOnline"].(int) + models.Roomconf[k].MaxOnline
+		for _, room := range chatRoom {
+			roomArr := strings.Split(room, "_")
+			roomConfNS := "RoomConfig:" + roomArr[2] + ":"
+
+			Online, _ := redis.IntMap(redisConn.Do("PUBSUB", "NUMSUB", room))
+			statis["online"] = statis["online"].(int) + Online[room]
+			MaxOnline, _ := redis.Int(redisConn.Do("GET", roomConfNS+"MaxOnline"))
+			statis["MaxOnline"] = statis["MaxOnline"].(int) + MaxOnline
+
 			roomInfo := make(map[string]int)
-			roomInfo["online"] = room.Len()
-			if models.Roomconf[k].Silence {
+			roomInfo["online"] = Online[room]
+			Silence, _ := redis.Bool(redisConn.Do("GET", roomConfNS+"Silence"))
+			if Silence {
 				roomInfo["Silence"] = 1
 			} else {
 				roomInfo["Silence"] = 0
 			}
-			roomInfo["MaxOnline"] = models.Roomconf[k].MaxOnline
-			rooms[k] = roomInfo
+			roomInfo["MaxOnline"] = MaxOnline
+			roomk, _ := strconv.Atoi(roomArr[2])
+			rooms[roomk] = roomInfo
+
 		}
 		statis["rooms"] = rooms
 	}
