c6a0cff1b732e1a547a2418446d5f34bba15b636
One2r <601941036@qq.com>
2017-07-12 10:28:55 +0800

    重写websocket_bench工具

--- tests/websocket_bench.go
+++ tests/websocket_bench.go
@@ -2,35 +2,65 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
+	"os"
 	"strconv"
 	"sync"
 	"time"
+
+	"github.com/gorilla/websocket"
+
+	cli "gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	for i := 0; i < 10000; i++ {
-		time.Sleep(10 * time.Millisecond)
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			dialer := websocket.Dialer{ /* set fields as needed */ }
-			ws, _, err := dialer.Dial("ws://192.168.190.131:8088?room=1&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0OTk4MjI4MzMsImlzcyI6ImNoYXRyb29tIiwibmJmIjoxNDk5NzM2NDMzLCJzdWIiOnsiSUQiOi0xLCJUeXBlIjoiYWRtaW4iLCJVc2VybmFtZSI6ImFkbWluIn19.4WCrjL6rlhP8Xih88KXSmYAJMk0r11cpd59gn3Rfobw", nil)
-			if err != nil {
-				// handle error
-			}
-			if err := ws.WriteMessage(websocket.TextMessage, []byte("i am : "+strconv.Itoa(i))); err != nil {
-				// handle error
-			}
-			for {
-				_, p, err := ws.ReadMessage()
+	app := cli.NewApp()
+	app.Name = "websocket_bench"
+	app.Usage = "A simple websocket bench tool"
+	app.Version = "0.0.1"
+	app.Author = "One2r"
+	app.Email = "601941036@qq.com"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "ws",
+			Value: "",
+			Usage: "websocket server url,eg: ws://192.168.1.190",
+		},
+		cli.IntFlag{
+			Name:  "c",
+			Value: 10,
+			Usage: "establish connections number",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		var wg sync.WaitGroup
+		if c.String("ws") == "" {
+			fmt.Println("error:Please input the websocket server url ")
+			return
+		}
+		for i := 0; i < c.Int("c"); i++ {
+			time.Sleep(10 * time.Millisecond)
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				dialer := websocket.Dialer{ /* set fields as needed */ }
+				ws, _, err := dialer.Dial(c.String("ws"), nil)
 				if err != nil {
 					// handle error
 				}
-				fmt.Println(strconv.Itoa(i)+" get message :", string(p))
-			}
-		}(i)
+				if err := ws.WriteMessage(websocket.TextMessage, []byte("I am No.:"+strconv.Itoa(i))); err != nil {
+					// handle error
+				}
+				for {
+					_, p, err := ws.ReadMessage()
+					if err != nil {
+						// handle error
+					}
+					fmt.Println("No.:"+strconv.Itoa(i)+" get message :", string(p))
+				}
+			}(i)
+		}
+		wg.Wait()
 	}
-	wg.Wait()
+	app.Run(os.Args)
 }
